b182a3654971063d6af3132fe8e3ee3e1e50f65a
Stephen Remillard <sremillard@pciauto.com>
2019-09-02 00:42:03 -0400

    PID Implementation, constants in config file

--- src/config.h
+++ src/config.h
@@ -10,16 +10,32 @@
 #define G_LED A1                            // Analog pin for the Green LED
 #define PIEZO_BUZZER 9                            // Analog pin for the Green LED
  
-#define SERVO_1_OFFSET 0                    // Used to compensate the servo #1  misalignment
-#define SERVO_2_OFFSET 0                    // Used to compensate the servo #2 misalignment
+
 #define APOGEE_DIFF_METERS 10               //  Used to specify minimum altitude for liftoff and minimum decent for parachute deployment. 
 #define EXCESSIVE_ANGLE_THRESHOLD 50         //  Used to specify maximum angle before abort sequence is initiated.
-#define BEEPER_ENABLED 1                     //  Used to specify if the beeped should be neabled or not.
-#define SCAN_TIME_INTERVAL 25                //  Used to specify the refresh rate in mili-seconds of the instruments (altimeter and gyroscope).
+#define SCAN_TIME_INTERVAL 10                //  Used to specify the refresh rate in mili-seconds of the instruments (altimeter and gyroscope).
 
-#define DEBUG 1                             // Set to 1 to display debug info to the serial console. Set to 0 otherwise.
+#define DEBUG 0                             // Set to 1 to display debug info to the serial console. Set to 0 otherwise.
 #define BUZZER_ENABLE 0                     // Set to 1 to enable the buzzer. Set to 0 otherwise.
 
+// SERVO STUFF
+#define SERVO_1_OFFSET 10                    // Used to compensate the servo #1  misalignment
+#define SERVO_2_OFFSET 0                    // Used to compensate the servo #1  misalignment
+#define SERVO_1_ORIENTATION 1               // Used to reverse the orientation of the servo #1 (possible values 1, -1) 
+#define SERVO_2_ORIENTATION 1               // Used to reverse the orientation of the servo #2 (possible values 1, -1)
+
+#define MAX_FINS_TRAVEL 25                  // Used to specify limits of the fins travel in degrees (+/-)
+
+
+// PITCH PID CONSTANTS
+#define PID_PITCH_Kp 4
+#define PID_PITCH_Ki 0
+#define PID_PITCH_Kd 0
+
+// ROLL PID CONSTANTS
+#define PID_ROLL_Kp 4
+#define PID_ROLL_Ki 0
+#define PID_ROLL_Kd 0
 
 
 #endif // CONFIG_FILE_H
--- src/servo/fins_servo.cpp
+++ src/servo/fins_servo.cpp
@@ -6,21 +6,21 @@ Servo servo_2;
 //Define Variables we'll be connecting to
 double Setpoint_Pitch, Input_Pitch, Output_Pitch, Setpoint_Roll, Input_Roll, Output_Roll;
 
-PID pitchPID(&Input_Pitch, &Output_Pitch, &Setpoint_Pitch,2,5,1, DIRECT);
-PID rollPID(&Input_Roll, &Output_Roll, &Setpoint_Roll,2,5,1, DIRECT);
+PID pitchPID(&Input_Pitch, &Output_Pitch, &Setpoint_Pitch, PID_PITCH_Kp, PID_PITCH_Ki, PID_PITCH_Kd, DIRECT);
+PID rollPID(&Input_Roll, &Output_Roll, &Setpoint_Roll, PID_ROLL_Kp, PID_ROLL_Ki, PID_ROLL_Kd, DIRECT);
 
 void setupServo() {
     servo_1.attach(FINS_SERVO_1_PIN);  // attaches the servo pin
-    servo_1.write(90);                  
+    servo_1.write(90 + SERVO_1_OFFSET);                  
     servo_2.attach(FINS_SERVO_2_PIN);  // attaches the servo on pin 9 to the servo object
-    servo_2.write(90);  
+    servo_2.write(90 + SERVO_2_OFFSET);  
 
     Setpoint_Pitch = 0;
     Setpoint_Roll = 0;
 
     //Specify the links and initial tuning parameters
-    pitchPID.SetOutputLimits(-90, 90);
-    rollPID.SetOutputLimits(-90, 90);
+    pitchPID.SetOutputLimits(MAX_FINS_TRAVEL *-1, MAX_FINS_TRAVEL);
+    rollPID.SetOutputLimits(MAX_FINS_TRAVEL *-1, MAX_FINS_TRAVEL);
     //turn the PID on
     pitchPID.SetMode(AUTOMATIC);
     rollPID.SetMode(AUTOMATIC);
@@ -40,65 +40,64 @@ void testServo() {
     servo_2.write(85);   
 }
 
-// OLD Direct Gyro-Servo connection
-void moveServo(float _ypr[]) {
-//ypr[0] * 180/M_PI
+// // OLD Direct Gyro-Servo connection
+// void moveServo(float _ypr[]) {
+// //ypr[0] * 180/M_PI
 
-    uint8_t pos_1;
-    uint8_t pos_2;
+//     uint8_t pos_1;
+//     uint8_t pos_2;
 
-    pos_1 =(uint8_t) 90-((_ypr[1] * 180/M_PI)*-1);
-    pos_1 = ((pos_1 < 5) ? 5 : pos_1);
-    pos_1 = ((pos_1 >= 175) ? 175 : pos_1);
+//     // pos_1 =(uint8_t) 90-((_ypr[1] * 180/M_PI)*-1);
+//     pos_1 =(uint8_t) 90-((_ypr[1] * 180/M_PI) * SERVO_1_ORIENTATION);
+//     pos_1 = ((pos_1 < 5) ? 5 : pos_1);
+//     pos_1 = ((pos_1 >= 175) ? 175 : pos_1);
 
-    pos_2 =(uint8_t) 90-((_ypr[2] * 180/M_PI));  // Reverse the sign
-    pos_2 = ((pos_2 < 5) ? 5 : pos_2);
-    pos_2 = ((pos_2 >= 175) ? 175 : pos_2);
+//     pos_2 =(uint8_t) 90-((_ypr[2] * 180/M_PI)  * SERVO_2_ORIENTATION); 
+//     pos_2 = ((pos_2 < 5) ? 5 : pos_2);
+//     pos_2 = ((pos_2 >= 175) ? 175 : pos_2);
 
-    Serial.print("servo-1: ");
-    Serial.print(pos_1);
-    Serial.print("   servo-2: ");
-    Serial.println(pos_2);
+//     Serial.print("servo-1: ");
+//     Serial.print(pos_1);
+//     Serial.print("   servo-2: ");
+//     Serial.println(pos_2);
     
-    servo_1.write(pos_1);  
-    servo_2.write(pos_2);  
-}
-
-
+//     servo_1.write(pos_1);  
+//     servo_2.write(pos_2);  
+// }
 
 
 
 // NEW PID Controlled Gyro-Servo loop
-// void moveServo(float _ypr[]) {
-// //ypr[0] * 180/M_PI
+void moveServo(float _ypr[]) {
+//ypr[0] * 180/M_PI
 
 
-//     int pos_1;
-//     int pos_2;
+    int pos_1;
+    int pos_2;
 
-//     pos_1 =(int) (_ypr[1] * 180/M_PI);
-//     pos_2 =(int) (_ypr[2] * 180/M_PI);
+    pos_1 =(int) (_ypr[1] * 180/M_PI);
+    pos_2 =(int) (_ypr[2] * 180/M_PI);
 
-//     Input_Pitch = pos_1;
-//     Input_Roll = pos_2;
+    Input_Pitch = pos_1;
+    Input_Roll = pos_2;
 
-//     pitchPID.Compute();
-//     rollPID.Compute();
+    pitchPID.Compute();
+    rollPID.Compute();
 
-//     Serial.print("pitchPID I: ");
-//     Serial.print(Input_Pitch);
-//     Serial.print("  O: ");
-//     Serial.println(Output_Pitch);
+    Serial.print("pitchPID I: ");
+    Serial.print(Input_Pitch);
+    Serial.print("  O: ");
+    Serial.println(Output_Pitch);
 
-//     Serial.print("rollhPID I: ");
-//     Serial.print(Input_Roll);
-//     Serial.print("  O: ");
-//     Serial.println(Output_Roll);
+    Serial.print("rollhPID I: ");
+    Serial.print(Input_Roll);
+    Serial.print("  O: ");
+    Serial.println(Output_Roll);
 
  
-//     servo_2.write(Output_Roll + 90); 
-//     servo_1.write(Output_Pitch + 90);  
+    servo_2.write(Output_Roll + 90); 
+    servo_1.write(Output_Pitch + 90);  
 
-// }
+}
 
 //@TODO: Make a PID control loop
