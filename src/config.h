7ad77c45478d848dec0523f8547a3a0e44f6a69a
Stephen Remillard <sremillard@pciauto.com>
2019-09-29 00:34:46 -0400

    PID Tuning

--- src/config.h
+++ src/config.h
@@ -13,32 +13,32 @@
 
 #define APOGEE_DIFF_METERS 10               //  Used to specify minimum altitude for liftoff and minimum decent for parachute deployment. 
 #define EXCESSIVE_ANGLE_THRESHOLD 50         //  Used to specify maximum angle before abort sequence is initiated.
-#define SCAN_TIME_INTERVAL 20                //  Used to specify the refresh rate in mili-seconds of the instruments (altimeter and gyroscope).
+#define SCAN_TIME_INTERVAL 25                //  Used to specify the refresh rate in mili-seconds of the instruments (altimeter and gyroscope).
 
 
 // SERVO STUFF
-#define SERVO_1_OFFSET 10                    // Used to compensate the servo #1  misalignment
-#define SERVO_2_OFFSET 0                    // Used to compensate the servo #1  misalignment
+#define SERVO_1_OFFSET 0                    // Used to compensate the servo #1 (Pitch) misalignment
+#define SERVO_2_OFFSET -5                    // Used to compensate the servo #1 (Roll) misalignment
 #define SERVO_1_ORIENTATION 1               // Used to reverse the orientation of the servo #1 (possible values 1, -1) 
 #define SERVO_2_ORIENTATION 1               // Used to reverse the orientation of the servo #2 (possible values 1, -1)
 
-#define MAX_FINS_TRAVEL 25                  // Used to specify limits of the fins travel in degrees (+/-)
+#define MAX_FINS_TRAVEL 50                  // Used to specify limits of the fins travel in degrees (+/-)
 
 
 // PITCH PID CONSTANTS
-#define PID_PITCH_Kp 3
+#define PID_PITCH_Kp 2
 #define PID_PITCH_Ki 0
-#define PID_PITCH_Kd 0
+#define PID_PITCH_Kd 0.5
 
 // ROLL PID CONSTANTS
-#define PID_ROLL_Kp 3
+#define PID_ROLL_Kp 2
 #define PID_ROLL_Ki 0
-#define PID_ROLL_Kd 0
+#define PID_ROLL_Kd 0.5
 
 #define DEBUG 0                             // Set to 1 to display debug info to the serial console. Set to 0 otherwise.
 #define BUZZER_ENABLE 0                     // Set to 1 to enable the buzzer. Set to 0 otherwise.
 
-#define DATA_RECOVERY_MODE 0                // Set to 1 to read collected data from memory
+#define DATA_RECOVERY_MODE 0                // Set to 1 to read collected data from memory: 0 to save data to memory
 #define FORMAT_MEMORY 1                     // Set to 1 to erase memory.
 
 #endif // CONFIG_FILE_H
