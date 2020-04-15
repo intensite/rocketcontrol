466b21bab1a7ea1e532bf6d682c0b11b29002f44
Stephen Remillard <sremillard@pciauto.com>
2020-04-14 23:58:56 -0400

    Fixed bug with the RGB Led

--- src/led_color/led_color.cpp
+++ src/led_color/led_color.cpp
@@ -4,7 +4,7 @@ void led_color(byte color_code) {
 
     // Used the bits of the color code to turn on or off different color leds (Total 8 colors)
     (color_code &  0x1 )  ? digitalWrite(B_LED, LOW) : digitalWrite(B_LED, HIGH);
-    (color_code &  0x2 )  ? digitalWrite(G_LED, LOW) : digitalWrite(B_LED, HIGH);
-    (color_code &  0x3 )  ? digitalWrite(R_LED, LOW) : digitalWrite(B_LED, HIGH);
+    (color_code &  0x2 )  ? digitalWrite(G_LED, LOW) : digitalWrite(G_LED, HIGH);
+    (color_code &  0x3 )  ? digitalWrite(R_LED, LOW) : digitalWrite(R_LED, HIGH);
 
 }
